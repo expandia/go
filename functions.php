fdaba6f5172adea78e10deb97df4344c95195a45
samueldsquire <sam_squire@msn.com>
2017-03-18 22:49:50 +0000

    Scripts and styles in child functions.php so rmvd
    Signed-off-by: samueldsquire <sam_squire@msn.com>

--- functions.php
+++ functions.php
@@ -20,28 +20,6 @@ function register_my_menu() {
 add_action( 'init', 'register_my_menu' );
 
 
-
-// Dequeue Scripts
-
-
-
-// Enqueue Scripts
-add_action( 'wp_enqueue_scripts', 'expandia_enqueue_scripts' );
-
-function expandia_enqueue_scripts() {
-  wp_register_script( 'main', get_template_directory_uri() . '/js/main-min.js', array('jquery'), '1', false );
-  wp_enqueue_script( 'main' );
-
-}
-
-// Register Style
-function custom_styles() {
-	wp_register_style( 'styles', get_template_directory_uri() . '/style.css', false, false, 'all' );
-	wp_enqueue_style( 'styles' );
-}
-add_action( 'wp_enqueue_scripts', 'custom_styles' );
-
-
 // Minify HTML
 class WP_HTML_Compression
 {
